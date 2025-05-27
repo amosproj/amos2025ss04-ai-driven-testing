bd9841030ed8a89374c0f4c8e4ce01228979e2e4
Lisabeth Hasse <lisabeth.hasse@gmail.com>
2025-05-27 22:29:24 +0200

    update environment.yml and add file to run the include_project.py module more easily

---
+++ backend/run_include_project.py
@@ -0,0 +1,15 @@
+from modules.include_project import IncludeProject
+
+
+
+if __name__ == "__main__":
+    # to run directly, independant of main.py
+    prompt_data = {
+        "model_index": 0,
+        "reset": True,
+        "prompt": "can you tell me the licence of the package.json and give me his dependencies?",  # This prompt tests if the github link has been sucessfully cloned.
+        # "prompt": "Create a unit-test for the function in python-test-cases called test_case_two.py" # This prompt checks if the local files have been included correctly.
+    }
+    module = IncludeProject()
+    result = module.process_prompt(prompt_data)
+    print("RAG response:", result.get("rag_response"))
